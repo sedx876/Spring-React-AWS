9afad79639c360de72c6237d63b0f069556d7a39
sedx876 <swatkins876@gmail.com>
2021-05-14 11:42:06 -0400

    Spring spinup backend

---
+++ aws-image-upload/src/test/java/com/sw/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
